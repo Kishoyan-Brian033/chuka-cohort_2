8dfda2b21724d50ce549c51e44b004b2b9034c8d
Brian Mwangi <brianmwangiat033@gmail.com>
2025-05-06 15:42:11 +0300

    added java file

---
+++ JAVA/app.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Declare and initialize two numbers
+        int firstNumber = 5;
+        int secondNumber = 7;
+        
+        // Calculate the sum
+        int sum = firstNumber + secondNumber;
+        
+        // Print the result
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+    }
+}
